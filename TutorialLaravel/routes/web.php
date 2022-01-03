a924714f6da3a2878657f24a74d5f16f924c7897
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2022-01-03 12:34:52 -0300

    3º dia
    Agora vai começar a parte boa
    
    https://www.youtube.com/watch?v=376vZ1wNYPA&ab_channel=CodeWithDary
    
    1:34:00

--- TutorialLaravel/routes/web.php
+++ TutorialLaravel/routes/web.php
@@ -12,7 +12,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Route that sends back a view
 Route::get('/', function () {
-    return env('CREATOR_NAME');
+    return view('welcome');
+});
+
+// Route to user - string
+Route::get('/users', function () {
+    return 'Welcome to the user space';
+});
+
+//Route to users - Array(JSON)
+Route::get('/users', function () {
+    return ['PHP', 'HTML', 'Laravel'];
+});
+
+
+//Route to users - JSON object
+Route::get('/users', function (){
+     return response()->json([
+         'name' => 'Kennedy',
+         'course' => 'Lravel Begginers To Advanced'
+     ]);
+});
+
+//Route to users - function
+Route::get('/users', function (){
+     return redirect('/');
 });
