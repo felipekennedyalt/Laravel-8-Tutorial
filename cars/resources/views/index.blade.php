69b445f210e26e2ca45c3c39982c3756ffd8271b
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2022-02-03 10:58:07 -0300

    aula e comandos
    comandos:
    
    composer create-project laravel/laravel "nome do projeto"
    
    (pra instalar o bootstrap é assim)
    composer require laravel/ui
    php artisan ui bootstrap
    npm install
    npm run dev
    (aqui é pra instalar o tailwind)
    composer require laravel-frontend-presets/tailwindcss --dev
    
    npm install && npm run dev
    
    php artisan make:model Car -m
    
    php artisan make:controller  CarsController --resource
    (esse resource cria o crud no controller)
    
    php artisan route:list
    
    (no body da view de layout)
    @yield('content')
    
    (link do strylesheet href)
    {{ asset('css/app.css') }}
    
    (extend no index)
    @extends('layouts.app')

---
+++ cars/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="m-auto w-4/5 py-24">
+    <div class="text-right">
+        <h1 class="text-50xl lowercase bold">
+            CARS
+        </h1>
+    </div>
+</div>
