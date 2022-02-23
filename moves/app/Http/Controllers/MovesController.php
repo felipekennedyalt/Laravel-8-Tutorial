cf2039f20221b3d9e5e1bb5d0217a8bce8095755
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2022-02-23 12:51:29 -0300

    rota, migration, factory, controller, model
    criação das classes acima

---
+++ moves/app/Http/Controllers/MovesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Moves;
+use Illuminate\Http\Request;
+
+class MovesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Moves  $moves
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Moves $moves)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Moves  $moves
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Moves $moves)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Moves  $moves
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Moves $moves)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Moves  $moves
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Moves $moves)
+    {
+        //
+    }
+}
